cff0a3960c71fb8af27c1568762f777628d5e95f
huobiapi <huobiapi@huobi.com>
2014-11-25 18:28:49 +0800

    Update FuturesMain.py

--- Futures/FuturesMain.py
+++ Futures/FuturesMain.py
@@ -15,19 +15,19 @@ FUTURES_CANCEL_ORDER = "https://api.bitvc.com/futures/order/cancel"
 if __name__ == "__main__":
     print "获取期货资产信息"
     print getBalanceInfo(1,FUTURES_BALANCE_INFO)
-    #print "获取用户持仓记录，week 周 quarter 季"
-    #print getHoldOrderList(1,"week",FUTURES_HOLD_ORDER_LIST)
-    #print "获取用户持仓记录，汇总"
-    #print getHoldOrderSum(1,"week",FUTURES_HOLD_ORDER_SUM)
+    print "获取用户持仓记录，week 周 quarter 季"
+    print getHoldOrderList(1,"week",FUTURES_HOLD_ORDER_LIST)
+    print "获取用户持仓记录，汇总"
+    print getHoldOrderSum(1,"week",FUTURES_HOLD_ORDER_SUM)
     print "获取所有正在进行的委托"
     print getOrderList(1,"week",FUTURES_ORDERS_LIST)
-    #print "委托单详情"
-    #print getOrderInfo(3757870,1,"week",FUTURES_ORDER_INFO)
-    #print "下委托单"
+    print "委托单详情"
+    print getOrderInfo(3757870,1,"week",FUTURES_ORDER_INFO)
+    print "下委托单"
     price=2470
     money=100
     #1开2平，1买2卖
-    #print saveOrder(1,"week",1,1,price,money,10,FUTURES_ORDER_SAVE)
-    #print "取消订单接口"
-    #print cancelOrder(1,"week",3757870,FUTURES_CANCEL_ORDER)
+    print saveOrder(1,"week",1,1,price,money,10,FUTURES_ORDER_SAVE)
+    print "取消订单接口"
+    print cancelOrder(1,"week",3757870,FUTURES_CANCEL_ORDER)
 
