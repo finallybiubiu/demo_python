64d588f46449cae72e792cbcfc9eaf8a4aa04620
huobiapi <huobiapi@huobi.com>
2014-11-25 18:31:35 +0800

    Update Util.py

--- bitvc/Util.py
+++ bitvc/Util.py
@@ -5,9 +5,9 @@ import hashlib
 import hmac
 import base64
 
-
-ACCESS_KEY="28d3e07f-32c6c6d9-e83328c9-21f6fb65"
-SECRET_KEY="97aa8d28-a7b2c39e-1558b11d-8a5254e0"
+#在此输入您的Key
+ACCESS_KEY=""
+SECRET_KEY=""
 
 
 def signature(params):
